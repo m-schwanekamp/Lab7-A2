4bd194c291c0739e7afcd4c72e045b3d22685072
m-schwanekamp <112090940+m-schwanekamp@users.noreply.github.com>
2022-10-12 15:28:32 -0400

    Update Lab7-A2.cpp

--- Lab7-A2.cpp
+++ Lab7-A2.cpp
@@ -1,26 +1,79 @@
-//Lab7-A2
+//Lab7-A1
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 
 using namespace std;
 
+
 int main(void) {
+	int movement;
+	int drunk = 1;
+	int numdrunk = 0;
+	int x = 0;
+	int y = 0;
 	int minutes;
-	int numsteps;
-	double distance;
 
 	cout << "Enter number of minutes for each drunkard to stagger: ";
 	cin >> minutes;
-
 	cout << endl;
 
-	srand((unsigned) time(0));
-	for (int second = 1; second <= minutes * 60; second++) {
-	
+	srand(time(0));
+	while (drunk < 10) {
+		numdrunk++;
+		drunk = numdrunk;
+		cout << "Drunkard #" << drunk;
+
+
+		for (int m = 0; m <= 8; m++) {
+			movement = rand() % 9;
+		}
+
+		//generate location of x and y coordinate
+		if (movement == 3) {
+			x = 0;
+			y++;
+		}
+		if (movement == 7) {
+			x = 0;
+			y--;
+		}
+		if (movement == 4) { 
+			x++;
+			y++;
+		}
+		if (movement == 5) {
+			x++;
+			y = 0;
+		}
+		if (movement == 8) {
+			x++;
+			y--;
+		}
+		if (movement == 1) {
+			x--;
+			y = 0;
+		}
+		if (movement == 2) {
+			x--;
+			y++;
+		}
+		if (movement == 6) {
+			x--;
+			y--;
+		}
+		else if (movement == 0) {
+			x = 0;
+			y = 0;
+		}
+		cout << " finished at location (" << x << ", " << y << ")" << endl;
 	}
 
-	//expected final distance
-	distance = sqrt(numsteps * 2.1);
+	//Calculate step distance
+	int numSteps = 0;
+	double distance;
+	numSteps = x * y;
+	distance = sqrt(numSteps * 2.1);
 	cout << "Expected distance: " << distance << endl;
+
 }
